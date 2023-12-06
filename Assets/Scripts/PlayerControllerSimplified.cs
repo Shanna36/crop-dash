980a350ab2d7f55a5e8dbb2741e804a25ac39803
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-12-06 11:09:46 +0000

    Wheels Animated
    Wheels animated for steering and rotaiton

--- Assets/Scripts/PlayerControllerSimplified.cs
+++ Assets/Scripts/PlayerControllerSimplified.cs
@@ -1,88 +1,118 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 public class PlayerControllerSimplified : MonoBehaviour
 {
-    
     public List<AxleInfo> axleInfos; // the information about each individual axle
     public float maxMotorTorque; // maximum torque the motor can apply to wheel
     public float maxSteeringAngle; // maximum steer angle the wheel can have
 
-    public KeyCode switchKey; 
+    public KeyCode switchKey;
     public Camera sideViewCamera;
     public Camera topCamera;
 
     private Rigidbody playerRb;
-    public GameObject centerOfMass; 
+    public GameObject centerOfMass;
 
-    public Slider fillBar; 
+    public Slider fillBar;
 
-    public bool isUnloading; 
+    public bool isUnloading;
 
-    public Vector3 centerOfMassOffset; 
+    public Vector3 centerOfMassOffset;
 
-        [System.Serializable]
-         public class AxleInfo
-    {   
-        public WheelCollider leftWheel;
-        public WheelCollider rightWheel;
+    public float steeringSpeed = 2.5f; 
+
+    [System.Serializable]
+    public class AxleInfo
+    {
+        public WheelCollider leftWheelCollider;
+        public WheelCollider rightWheelCollider;
         public bool motor; // is this wheel attached to motor?
         public bool steering; // does this wheel apply steer angle?
-    }
+        public Transform leftWheelMesh;
+        public Transform rightWheelMesh;
 
+       
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.centerOfMass = centerOfMass.transform.localPosition + centerOfMassOffset; 
-        isUnloading = false; 
-
+        playerRb.centerOfMass = centerOfMass.transform.localPosition + centerOfMassOffset;
+        isUnloading = false;
     }
-    
 
     // Update is called once per frame
     void Update()
     {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
         if (Input.GetKeyDown(switchKey))
         {
             sideViewCamera.enabled = !sideViewCamera.enabled;
-            topCamera.enabled = !topCamera.enabled; 
+            topCamera.enabled = !topCamera.enabled;
         }
-    }
-    
-    void FixedUpdate()
-    {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
-        
-        foreach (AxleInfo axleInfo in axleInfos) 
+
+         foreach (AxleInfo axleInfo in axleInfos) //moved to update to fix jitter
         {
-            if (axleInfo.steering) 
+            if (axleInfo.steering)
             {
-                axleInfo.leftWheel.steerAngle = steering;
-                axleInfo.rightWheel.steerAngle = steering;
+                axleInfo.leftWheelCollider.steerAngle = steering;
+                axleInfo.rightWheelCollider.steerAngle = steering;
             }
-            if (axleInfo.motor) 
+            if (axleInfo.motor)
             {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
+                axleInfo.leftWheelCollider.motorTorque = motor;
+                axleInfo.rightWheelCollider.motorTorque = motor;
             }
+
+            // Rotate the wheel meshes for the current axle
+        RotateWheelMesh(axleInfo.leftWheelCollider, axleInfo.leftWheelMesh, axleInfo.steering);
+        RotateWheelMesh(axleInfo.rightWheelCollider, axleInfo.rightWheelMesh, axleInfo.steering);
         }
     }
-
-
-
-   void OnTriggerEnter(Collider other)
-    {
     
+void RotateWheelMesh(WheelCollider wheelCollider, Transform wheelMesh, bool isSteering)
+{
+        Vector3 currentRotation = wheelMesh.localEulerAngles;
+
+    if (isSteering)
+    {
+        // Apply steering rotation (around the Y-axis)
+        float steerAngle = wheelCollider.steerAngle;
+        steerAngle = Mathf.Clamp(steerAngle, -maxSteeringAngle, maxSteeringAngle);
+        currentRotation.y = steerAngle;
+    }
+    else
+    {
+        // Fix the Z rotation for back wheels
+        currentRotation.y = 0;
+        currentRotation.z = 0;
+    }
+
+    // Apply rotation for wheel spinning (around the X-axis)
+    float rotationAngle = wheelCollider.rpm / 60 * 360 * Time.deltaTime;
+    currentRotation.x += rotationAngle;
+
+    // Fix the rotation on the Z-axis to zero
+    currentRotation.z = 0;
+
+    wheelMesh.localEulerAngles = currentRotation;
+}
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
         if (other.CompareTag("Wheat") && fillBar.value < 300)
         {
             Destroy(other.gameObject);
-            //slider logic for fill bar
-             fillBar.value += 1;
+            // slider logic for fill bar
+            fillBar.value += 1;
         }
 
         if (other.CompareTag("Tractor_Trailer"))
@@ -91,16 +121,14 @@ public class PlayerControllerSimplified : MonoBehaviour
         }
     }
 
-
-void OnDrawGizmos()
-{
-    if (!playerRb)
+    void OnDrawGizmos()
     {
-        playerRb = GetComponent<Rigidbody>();
+        if (!playerRb)
+        {
+            playerRb = GetComponent<Rigidbody>();
+        }
+        Gizmos.color = Color.red;
+        // Draw a red sphere at the transformed center of mass
+        Gizmos.DrawSphere(transform.position + transform.TransformPoint(playerRb.centerOfMass), 0.1f);
     }
-    Gizmos.color = Color.red;
-    // Draw a red sphere at the transformed center of mass
-    Gizmos.DrawSphere(transform.position + transform.TransformPoint(playerRb.centerOfMass), 0.1f);
-}
-
 }
